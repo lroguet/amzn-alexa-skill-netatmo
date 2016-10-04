cb8a68b32fee0f7527c03e0540ea8446a9d75573
Ludovic Roguet <ludovic.roguet@nordcloud.se>
2016-10-04 09:22:48 +0200

    Using package.json instead of manually installing dependencies.

--- skill/build.sh
+++ skill/build.sh
@@ -3,10 +3,8 @@
 # Move closer to the code
 cd src/
 
-# Clean & install additional package(s)
-echo Cleaning up workspace && rm -rf node_modules
-echo Installing alexa-sdk && npm install alexa-sdk
-echo Installing jmespath && npm install jmespath
+# Install additional packages
+npm install
 
 # Compress the Lambda function
 zip -r ../amzn-alexa-skill-netatmo.zip .
